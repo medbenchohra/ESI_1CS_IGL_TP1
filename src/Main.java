e997664ede799b520c79a7fdd0e58fe04c1e61f6
san000 <fm_sannef@esi.dz>
2017-10-13 17:35:48 +0200

    setting 0

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
 
     public int function () {
-        return 595;
+        return 0;
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
 }
