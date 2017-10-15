48adccecbbaafc5fcbdcc67b7b0a4fbcf3abab56
san000 <fm_sannef@esi.dz>
2017-10-15 15:48:22 +0200

    adding the min/max method (plus its helper class)

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -2,4 +2,24 @@
  * Created by Antoni on 15/10/2017.
  */
 public class VectorHelper {
+
+    public VectorInfo max_minVector (int[] a){
+        int max,min;
+        max = min = a[0];
+        for (int i=1;i<a.length;i++){
+            if (max < a[i]) max = a[i];
+            else if (min > a[i]) min = a[i];
+        }
+        return new VectorInfo(min,max);
+    }
+}
+
+class VectorInfo{
+    public static int min = 0;
+    public static int max = 0;
+
+    VectorInfo(int min,int max){
+        this.min = min;
+        this.max = max;
+    }
 }
