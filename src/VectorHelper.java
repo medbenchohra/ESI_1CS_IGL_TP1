18259f0239b2028fbfdba22e69ee59c2cabcdc34
medbenchohra <fm_benchohra@esi.dz>
2017-10-22 15:46:39 +0100

    Mohamed: deleted that method

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -116,9 +116,4 @@ class VectorInfo{
         return ("\n Min = " + this.min + "\n Max = " + this.max + "\n");
     }
 
-    private void conflictSimulation(int c) {
-        int k = 0;
-        k = 6;
-        System.out.println("yes");
-    }
 }
